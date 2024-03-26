60feeb7fa59c9b0a5e839554759f45a4f4faec81
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2024-03-26 21:32:42 +0100

    Base for the EventCreation GUI

---
+++ app/src/main/java/com/github/se/gatherspot/ui/CreateEvent.kt
@@ -0,0 +1,12 @@
+package com.github.se.gatherspot.ui
+
+// GUI to create an event
+import androidx.compose.runtime.Composable
+import com.github.se.gatherspot.ui.navigation.NavigationActions
+
+
+@Composable
+fun CreateEvent(nav: NavigationActions) {
+  // Manages creating an event
+  // Fields
+}
