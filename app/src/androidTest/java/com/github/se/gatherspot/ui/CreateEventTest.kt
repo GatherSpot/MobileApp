890dd5f3273cdf89cb30036db6efc68446567224
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2024-04-02 22:00:59 +0200

    Add and correct some tests - clean code

--- app/src/androidTest/java/com/github/se/gatherspot/ui/CreateEventTest.kt
+++ app/src/androidTest/java/com/github/se/gatherspot/ui/CreateEventTest.kt
@@ -1,16 +1,11 @@
 package com.github.se.gatherspot.ui
 
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsEnabled
-import androidx.compose.ui.test.assertIsNotEnabled
-import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.gatherspot.model.EventViewModel
