e8dbece14bdffdf0eb6eec436427800be061aa0b
mmalonga <melvin.malonga-matouba@epfl.ch>
2024-04-07 17:48:38 +0200

    making UI test

--- app/src/androidTest/java/com/github/se/gatherspot/SelectInterestsScreenTest.kt
+++ app/src/androidTest/java/com/github/se/gatherspot/SelectInterestsScreenTest.kt
@@ -14,7 +14,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 
-@RunWith(AndroidJUnit4::AndroidJUnit4::)
+@RunWith(AndroidJUnit4::class)
 class SelectInterestsTest : TestCase(){
     @get:Rule val composeTestRule = createComposeRule()
 
