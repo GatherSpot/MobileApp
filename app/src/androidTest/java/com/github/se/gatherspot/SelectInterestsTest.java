4d7f04c6b3c9cf79da9bc696ddd7968cf1a5614f
mmalonga <melvin.malonga-matouba@epfl.ch>
2024-04-07 17:28:19 +0200

    making UI test

--- app/src/androidTest/java/com/github/se/gatherspot/SelectInterestsTest.java
+++ app/src/androidTest/java/com/github/se/gatherspot/SelectInterestsTest.java
@@ -1,42 +1,34 @@
 package com.github.se.gatherspot;
 
-import org.junit.Test;
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.se.gatherspot.MainActivity
+import com.github.se.gatherspot.screens.LoginScreen
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 public class SelectInterestsTest : TestCase() {
 @get:Rule val composeTestRule = createAndroidComposeRule<SelectInterestsScreen>()
 @get:Rule val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
-        @Test
-        fun buttonsAreCorrectlyDisplayed() {
+@Test
+    fun buttonsAreCorrectlyDisplayed() {
         ComposeScreen.onComposeScreen<SelectInterestsScreen>(composeTestRule) {
         // Test the UI elements
-        for (interest in interestsList)
+        for (interestButton in interestsList){
+        interestButton {
+            assertIsDisplayed()
+            assertHasClickAction()
+        }
+        }
         }
                 }
 
 }
 
 
-class LoginTest : TestCase() {
-@get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
-
-// The IntentsTestRule simply calls Intents.init() before the @Test block
-// and Intents.release() after the @Test block is completed. IntentsTestRule
-// is deprecated, but it was MUCH faster than using IntentsRule in our tests
-@get:Rule val intentsTestRule = IntentsTestRule(MainActivity::class.java)
-
-@Test
-  fun buttonsAreCorrectlyDisplayed() {
-          ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
-        // Test the UI elements
-        loginButton {
-        assertIsDisplayed()
-        assertHasClickAction()
-        }
-        signUpButton {
-        assertIsDisplayed()
-        assertHasClickAction()
-        }
-        }
-        }
