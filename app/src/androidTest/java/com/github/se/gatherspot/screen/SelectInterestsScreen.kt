2f470e2e5bdd2b821511a036d43459d52b9a1807
mmalonga <melvin.malonga-matouba@epfl.ch>
2024-04-07 14:50:50 +0200

    making UI test

--- app/src/androidTest/java/com/github/se/gatherspot/screen/SelectInterestsScreen.kt
+++ app/src/androidTest/java/com/github/se/gatherspot/screen/SelectInterestsScreen.kt
@@ -6,11 +6,20 @@ import io.github.kakaocup.compose.node.element.KNode
 
 
 class SelectInterestsScreen (semanticsProvider: SemanticsNodeInteractionsProvider) :
-    ComposeScreen<SelectInterestsScreen.LoginScreen>(
+    ComposeScreen<SelectInterestsScreen>(
         semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("selectInterestsScreen") }){
 
-        val interestsList = KNode { hasTestTag("interestsList") }
+        val interestsList :KNode = child { hasTestTag("interestsList") }
 
+}
+
+class LoginScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<LoginScreen>(
+        semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("loginScreen") }) {
+
+    // Structural elements of the UI
+    val loginButton: KNode = child { hasTestTag("loginButton") }
+    val signUpButton: KNode = child { hasTestTag("signUpButton") }
 }
 
 
