eda98e7d759ee2e97ed22c4a4a05ccdef92e4011
Benoît MORAWIEC <78908194+B-mellon@users.noreply.github.com>
2024-04-12 15:08:37 +0200

    geomap 'tests'

--- app/src/androidTest/java/com/github/se/gatherspot/ui/GeoMapTest.kt
+++ app/src/androidTest/java/com/github/se/gatherspot/ui/GeoMapTest.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.assertAny
+import androidx.compose.ui.test.filter
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onChildren
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.unit.dp
 import com.github.se.gatherspot.model.location.Location
@@ -22,22 +25,44 @@ class GeoMapTest {
     val modifier = Modifier.fillMaxWidth().height(300.dp).padding(4.dp)
 
     composeTestRule.setContent { GeoMap(userCoordinates = null, interestsCoordinates) }
-
     composeTestRule.onNodeWithTag("Google Map").assertExists()
   }
 
   @Test
-  fun areInterestDisplayed() {
-    val userCoordinates = LatLng(46.518567549767575, 6.568562923656716)
-    val interestCoordinates =
-        listOf(
-            Location(46.52464786510155, 6.575147894055152, "Vortex"),
-            Location(46.51878838760822, 6.5619011030383, "IC BC"),
-            Location(46.523127173515185, 6.564655092362486, "swiss tech convention center"))
+  fun isMapDisplayedTest_WithoutParameters() {
+    val userCoordinates = null
+    val interestsCoordinates = emptyList<Location>()
     val modifier = Modifier.fillMaxWidth().height(300.dp).padding(4.dp)
 
-    composeTestRule.setContent { GeoMap(userCoordinates = null, interestCoordinates, modifier) }
+    composeTestRule.setContent { GeoMap(userCoordinates = null, interestsCoordinates) }
+    composeTestRule.onNodeWithTag("Google Map").assertExists()
+  }
 
-    val map = composeTestRule.onNodeWithTag("Google Map")
+  @Test
+  fun isMapDisplayedTest_WithAllParameters() {
+    val userCoordinates = Location(46.518567549767575, 6.568562923656716, "")
+    val interestsCoordinates =         listOf(
+      Location(46.52464786510155, 6.575147894055152, "Vortex"),
+      Location(46.51878838760822, 6.5619011030383, "IC BC"),
+      Location(46.523127173515185, 6.564655092362486, "swiss tech convention center")
+    )
+    val modifier = Modifier.fillMaxWidth().height(300.dp).padding(4.dp)
+
+    composeTestRule.setContent { GeoMap(userCoordinates = null, interestsCoordinates) }
+    composeTestRule.onNodeWithTag("Google Map").assertExists()
+  }
+
+  @Test
+  fun isMapDisplayedTest_WithNoUserAndMap() {
+    val userCoordinates = null
+    val interestsCoordinates =         listOf(
+      Location(46.52464786510155, 6.575147894055152, "Vortex"),
+      Location(46.51878838760822, 6.5619011030383, "IC BC"),
+      Location(46.523127173515185, 6.564655092362486, "swiss tech convention center")
+    )
+    val modifier = Modifier.fillMaxWidth().height(300.dp).padding(4.dp)
+
+    composeTestRule.setContent { GeoMap(userCoordinates = null, interestsCoordinates) }
+    composeTestRule.onNodeWithTag("Google Map").assertExists()
   }
 }
