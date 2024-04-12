bb2cd8a9131a5248baa082018235aa72cc28ae1f
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2024-04-12 15:11:55 +0200

    ktfmt

--- app/src/androidTest/java/com/github/se/gatherspot/ui/GeoMapTest.kt
+++ app/src/androidTest/java/com/github/se/gatherspot/ui/GeoMapTest.kt
@@ -4,14 +4,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.test.assertAny
-import androidx.compose.ui.test.filter
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onChildren
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.unit.dp
 import com.github.se.gatherspot.model.location.Location
-import com.google.android.gms.maps.model.LatLng
 import org.junit.Rule
 import org.junit.Test
 
@@ -41,11 +37,11 @@ class GeoMapTest {
   @Test
   fun isMapDisplayedTest_WithAllParameters() {
     val userCoordinates = Location(46.518567549767575, 6.568562923656716, "")
-    val interestsCoordinates =         listOf(
-      Location(46.52464786510155, 6.575147894055152, "Vortex"),
-      Location(46.51878838760822, 6.5619011030383, "IC BC"),
-      Location(46.523127173515185, 6.564655092362486, "swiss tech convention center")
-    )
+    val interestsCoordinates =
+        listOf(
+            Location(46.52464786510155, 6.575147894055152, "Vortex"),
+            Location(46.51878838760822, 6.5619011030383, "IC BC"),
+            Location(46.523127173515185, 6.564655092362486, "swiss tech convention center"))
     val modifier = Modifier.fillMaxWidth().height(300.dp).padding(4.dp)
 
     composeTestRule.setContent { GeoMap(userCoordinates = null, interestsCoordinates) }
@@ -55,11 +51,11 @@ class GeoMapTest {
   @Test
   fun isMapDisplayedTest_WithNoUserAndMap() {
     val userCoordinates = null
-    val interestsCoordinates =         listOf(
-      Location(46.52464786510155, 6.575147894055152, "Vortex"),
-      Location(46.51878838760822, 6.5619011030383, "IC BC"),
-      Location(46.523127173515185, 6.564655092362486, "swiss tech convention center")
-    )
+    val interestsCoordinates =
+        listOf(
+            Location(46.52464786510155, 6.575147894055152, "Vortex"),
+            Location(46.51878838760822, 6.5619011030383, "IC BC"),
+            Location(46.523127173515185, 6.564655092362486, "swiss tech convention center"))
     val modifier = Modifier.fillMaxWidth().height(300.dp).padding(4.dp)
 
     composeTestRule.setContent { GeoMap(userCoordinates = null, interestsCoordinates) }
