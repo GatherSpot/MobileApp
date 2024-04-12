7d47900bf2d9c9f06cd5ea35231e5e885fd9f2d0
Benoît MORAWIEC <78908194+B-mellon@users.noreply.github.com>
2024-04-12 14:40:24 +0200

    use of existing type Location

--- app/src/androidTest/java/com/github/se/gatherspot/ui/GeoMapTest.kt
+++ app/src/androidTest/java/com/github/se/gatherspot/ui/GeoMapTest.kt
@@ -4,19 +4,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.assertAll
-import androidx.compose.ui.test.assertCountEquals
-import androidx.compose.ui.test.filter
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onChild
-import androidx.compose.ui.test.onChildren
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.unit.dp
-import com.github.se.gatherspot.screens.EventUIScreen
+import com.github.se.gatherspot.model.location.Location
 import com.google.android.gms.maps.model.LatLng
-import io.github.kakaocup.compose.node.element.ComposeScreen
 import org.junit.Rule
 import org.junit.Test
 
@@ -25,9 +17,9 @@ class GeoMapTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun isMapDisplayedTest(){
-        val userCoordinates = LatLng(46.518567549767575, 6.568562923656716)
-        val interestsCoordinates = emptyList<GeoMapInterestPoint>()
+    fun isMapDisplayedTest() {
+        val userCoordinates = Location(46.518567549767575, 6.568562923656716, "")
+        val interestsCoordinates = emptyList<Location>()
         val modifier = Modifier
             .fillMaxWidth()
             .height(300.dp)
@@ -44,12 +36,12 @@ class GeoMapTest {
     }
 
     @Test
-    fun areInterestDisplayed(){
+    fun areInterestDisplayed() {
         val userCoordinates = LatLng(46.518567549767575, 6.568562923656716)
         val interestCoordinates = listOf(
-            GeoMapInterestPoint(LatLng(46.52464786510155, 6.575147894055152), "Vortex"),
-            GeoMapInterestPoint(LatLng(46.51878838760822, 6.5619011030383), "IC BC"),
-            GeoMapInterestPoint(LatLng(46.523127173515185, 6.564655092362486), "swiss tech convention center")
+            Location(46.52464786510155, 6.575147894055152, "Vortex"),
+            Location(46.51878838760822, 6.5619011030383, "IC BC"),
+            Location(46.523127173515185, 6.564655092362486, "swiss tech convention center")
         )
         val modifier = Modifier
             .fillMaxWidth()
@@ -64,6 +56,6 @@ class GeoMapTest {
             )
         }
 
-        val map =  composeTestRule.onNodeWithTag("Google Map")
+        val map = composeTestRule.onNodeWithTag("Google Map")
     }
 }
--- app/src/main/java/com/github/se/gatherspot/ui/GeoMap.kt
+++ app/src/main/java/com/github/se/gatherspot/ui/GeoMap.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import com.github.se.gatherspot.model.location.Location
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -13,29 +14,27 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
-data class GeoMapInterestPoint(val latLng: LatLng, val title: String)
-
 @Composable
 fun GeoMap(
-    userCoordinates: LatLng?,
-    interestsCoordinates: List<GeoMapInterestPoint>,
-    mapViewModifier: Modifier = Modifier
+  userCoordinates: Location?,
+  interestsCoordinates: List<Location>,
+  mapViewModifier: Modifier = Modifier
 ) {
   // find the camera initial position (default is Rolex Learning Center at EPFL)
   var cameraCenter = LatLng(46.518567549767575, 6.568562923656716)
 
   if (userCoordinates != null) {
-    cameraCenter = userCoordinates
+    cameraCenter = LatLng(userCoordinates.latitude, userCoordinates.longitude)
   } else {
     // compute the middle geolocation of the points
     // so that they are all visible on the map
     if (interestsCoordinates.isNotEmpty()) {
-      val latOnly = interestsCoordinates.map { it.latLng.latitude }
+      val latOnly = interestsCoordinates.map { it.latitude }
       val minLat = latOnly.min()
       val maxLat = latOnly.max()
       val centerLat = (minLat + maxLat) / 2
 
-      val longOnly = interestsCoordinates.map { it.latLng.longitude }
+      val longOnly = interestsCoordinates.map { it.longitude }
       val minLong = longOnly.min()
       val maxLong = longOnly.max()
       val centerLong = (minLong + maxLong) / 2
@@ -58,11 +57,14 @@ fun GeoMap(
         properties = MapProperties(mapType = MapType.HYBRID),
         modifier = mapViewModifier.testTag("Google Map")) {
           if (userCoordinates != null) {
-            Marker(state = MarkerState(position = userCoordinates), title = "Your current position")
+            Marker(state = MarkerState(position = LatLng(userCoordinates.latitude, userCoordinates.longitude)), title = "Your current position")
           }
 
           for (interest in interestsCoordinates) {
-            Marker(state = MarkerState(position = interest.latLng), title = interest.title)
+            Marker(state = MarkerState(
+              position = LatLng(interest.latitude, interest.longitude)),
+              title = interest.name
+            )
           }
         }
   }
