2b9da4918ab2e030535731fb8512b39a5f7a2b5f
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2024-04-17 13:45:06 +0200

    Test works in local environment

--- app/src/androidTest/java/com/github/se/gatherspot/model/EventRegistrationViewModelTest.kt
+++ app/src/androidTest/java/com/github/se/gatherspot/model/EventRegistrationViewModelTest.kt
@@ -20,11 +20,11 @@ class EventRegistrationViewModelTest {
 
     val event =
         Event(
-            id = "1",
+            id = "testEventId",
             title = "Event Title",
             description =
                 "Hello: I am a description of the event just saying that I would love to say that Messi is not the best player in the world, but I can't. I am sorry.",
-            attendanceMaxCapacity = 10,
+            attendanceMaxCapacity = 5,
             attendanceMinCapacity = 1,
             categories = setOf(Interests.BASKETBALL),
             eventEndDate = LocalDate.of(2024, 4, 15),
@@ -34,8 +34,8 @@ class EventRegistrationViewModelTest {
             inscriptionLimitTime = LocalTime.of(23, 59),
             location = null,
             registeredUsers = mutableListOf(),
-            timeBeginning = LocalTime.of(13, 0),
-            timeEnding = LocalTime.of(16, 0),
+            timeBeginning = LocalTime.of(10, 0),
+            timeEnding = LocalTime.of(12, 0),
         )
 
     runBlocking { viewModel.registerForEvent(event) }
