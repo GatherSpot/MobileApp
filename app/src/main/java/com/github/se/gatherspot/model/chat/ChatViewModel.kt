7da2323bab51f210beb67a7df35a364bb4a212f7
Elias Mir <elias.mir.2004@gmail.com>
2024-04-18 07:56:59 +0200

    basics of chat implementation done

--- app/src/main/java/com/github/se/gatherspot/model/chat/ChatViewModel.kt
+++ app/src/main/java/com/github/se/gatherspot/model/chat/ChatViewModel.kt
@@ -2,10 +2,10 @@ package com.github.se.gatherspot.model.chat
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.se.gatherspot.ChatFirebaseConnection
 import com.github.se.gatherspot.MainActivity
 import com.github.se.gatherspot.ProfileFirebaseConnection
 import com.github.se.gatherspot.model.Profile
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -20,9 +20,13 @@ class ChatViewModel : ViewModel() {
 
   init {
     viewModelScope.launch {
-      val nextChats =
-          (ProfileFirebaseConnection.fetch(FirebaseAuth.getInstance().currentUser!!.uid) as Profile)
-              .chats
+      // val nextChats =
+      //    (ProfileFirebaseConnection.fetch(FirebaseAuth.getInstance().currentUser!!.uid) as
+      // Profile)
+      //        .chats
+      // TODO in real app, we would use the commented code above
+      val chatFirebase = ChatFirebaseConnection()
+      val nextChats = chatFirebase.fetchNextChats(5)
       _uiState.value =
           ChatUIState(
               nextChats
