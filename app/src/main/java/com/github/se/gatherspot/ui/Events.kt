208c01f47a5e11aca88cb6d3e48e2f753cb6753e
Wesley Nana Davies <92039461+WesLeeO@users.noreply.github.com>
2024-04-19 11:46:54 +0200

    Filter events (#78)
    * filter feature implemented
    
    * ui tests written for filtering
    
    * resolve conflicts

--- app/src/main/java/com/github/se/gatherspot/ui/Events.kt
+++ app/src/main/java/com/github/se/gatherspot/ui/Events.kt
@@ -1,11 +1,15 @@
 package com.github.se.gatherspot.ui
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,8 +19,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -40,6 +49,7 @@ import androidx.compose.ui.unit.dp as dp
 import androidx.compose.ui.unit.sp
 import com.github.se.gatherspot.R
 import com.github.se.gatherspot.model.EventsViewModel
+import com.github.se.gatherspot.model.Interests
 import com.github.se.gatherspot.model.event.Event
 import com.github.se.gatherspot.model.event.EventStatus
 import com.github.se.gatherspot.ui.navigation.BottomNavigationMenu
@@ -50,12 +60,19 @@ import java.time.format.DateTimeFormatter
 import kotlinx.coroutines.delay
 
 /** Composable that displays events * */
+
+// listOf("Your interests", "None")
 @Composable
 fun Events(viewModel: EventsViewModel, nav: NavigationActions) {
+
   val state = viewModel.uiState.collectAsState()
   var previousScrollPosition by remember { mutableIntStateOf(0) }
   var loading by remember { mutableStateOf(false) }
   var fetched by remember { mutableStateOf(false) }
+  var showDropdownMenu by remember { mutableStateOf(false) }
+  val filters = enumValues<Interests>().toList()
+  var interestsSelected = mutableListOf<Interests>()
+  var fetch by remember { mutableStateOf(false) }
 
   Scaffold(
       modifier = Modifier.testTag("EventsScreen"),
@@ -68,11 +85,52 @@ fun Events(viewModel: EventsViewModel, nav: NavigationActions) {
                 Text(
                     text = "Filter events", fontSize = 18.sp, modifier = Modifier.testTag("filter"))
                 Spacer(modifier = Modifier.width(10.dp))
+                Box {
+                  Icon(
+                      imageVector = Icons.Filled.Menu,
+                      contentDescription = null,
+                      modifier =
+                          Modifier.clickable {
+                                showDropdownMenu = !showDropdownMenu
+                                if (!showDropdownMenu) {
+                                  viewModel.filter(interestsSelected)
+                                  interestsSelected = mutableListOf()
+                                }
+                              }
+                              .testTag("filterMenu"))
+                  DropdownMenu(
+                      modifier = Modifier.height(300.dp).testTag("dropdown"),
+                      expanded = showDropdownMenu,
+                      onDismissRequest = {
+                        showDropdownMenu = false
+                        viewModel.filter(interestsSelected)
+                        interestsSelected = mutableListOf()
+                      }) {
+                        DropdownMenuItem(
+                            text = { Text("REMOVE FILTER") },
+                            onClick = {
+                              viewModel.removeFilter()
+                              showDropdownMenu = false
+                            },
+                            leadingIcon = { Icon(Icons.Filled.Clear, "clear") })
+
+                        filters.forEach { s -> StatefulDropdownItem(s, interestsSelected) }
+                      }
+                }
+                Spacer(modifier = Modifier.width(30.dp))
                 Icon(
-                    imageVector = Icons.Filled.Menu,
-                    contentDescription = null,
-                    modifier = Modifier.clickable {}.testTag("filterMenu"))
-                Spacer(modifier = Modifier.width(80.dp))
+                    Icons.Filled.Refresh,
+                    "fetch",
+                    modifier = Modifier.clickable { fetch = true }.testTag("refresh"))
+                LaunchedEffect(fetch) {
+                  if (fetch) {
+                    Log.d(TAG, "entered")
+                    viewModel.fetchNext()
+                    fetch = false
+                  }
+                }
+
+                Spacer(modifier = Modifier.width(30.dp))
                 Text(
                     text = "Create an event",
                     fontSize = 18.sp,
@@ -100,10 +158,16 @@ fun Events(viewModel: EventsViewModel, nav: NavigationActions) {
             tabList = TOP_LEVEL_DESTINATIONS,
             selectedItem = nav.controller.currentBackStackEntry?.destination?.route)
       }) { paddingValues ->
+        if (fetch) {
+          Text(
+              modifier = Modifier.testTag("fetch").padding(vertical = 30.dp),
+              text = "Fetching new events...")
+        }
         val events = state.value.list
         val lazyState = rememberLazyListState()
+        Log.d(TAG, "size = " + events.size.toString())
         when {
-          events.isEmpty() -> Empty()
+          events.isEmpty() -> Empty(viewModel)
           else -> {
             LazyColumn(
                 state = lazyState,
@@ -129,13 +193,6 @@ fun Events(viewModel: EventsViewModel, nav: NavigationActions) {
           }
         }
       }
-}
-
-@Composable
-fun Empty() {
-  Row(modifier = Modifier.padding(vertical = 40.dp).testTag("empty")) {
-    Text(text = "Loading...", color = Color.Black)
-  }
 }
 
 @Composable
@@ -187,4 +244,40 @@ fun EventRow(event: Event, navigation: NavigationActions) {
         }
       }
   Divider(color = Color.Black, thickness = 1.dp)
+}
+
+@Composable
+fun Empty(viewModel: EventsViewModel) {
+  Box(modifier = Modifier.fillMaxSize().testTag("empty"), contentAlignment = Alignment.Center) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+      Text("No events matched your query")
+      Text(
+          "Remove filter",
+          color = Color.Blue,
+          modifier = Modifier.clickable { viewModel.removeFilter() })
+    }
+  }
+}
+
+@Composable
+fun StatefulDropdownItem(interest: Interests, interestsSelected: MutableList<Interests>) {
+  var selected by remember { mutableStateOf(false) }
+
+  DropdownMenuItem(
+      modifier = Modifier.testTag(interest.toString()),
+      text = { Text(interest.toString()) },
+      onClick = {
+        selected = !selected
+        if (selected) {
+          interestsSelected.add(interest)
+        } else {
+          interestsSelected.remove(interest)
+        }
+      },
+      leadingIcon = {
+        if (selected) {
+          Icon(Icons.Filled.Done, "")
+        }
+      },
+  )
 }
