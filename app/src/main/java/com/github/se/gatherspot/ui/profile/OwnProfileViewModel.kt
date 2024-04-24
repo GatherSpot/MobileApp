648219486df7cb5507325c349fdffe0332282c74
mmalonga <melvin.malonga-matouba@epfl.ch>
2024-04-24 16:31:48 +0200

    Replaced dummy fetching with actual fetching

--- app/src/main/java/com/github/se/gatherspot/ui/profile/OwnProfileViewModel.kt
+++ app/src/main/java/com/github/se/gatherspot/ui/profile/OwnProfileViewModel.kt
@@ -7,6 +7,14 @@ import androidx.lifecycle.ViewModel
 import com.github.se.gatherspot.ProfileFirebaseConnection
 import com.github.se.gatherspot.model.Interests
 import com.github.se.gatherspot.model.Profile
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 // Note : This warning is properly taken care of
 @SuppressLint("MutableCollectionMutableState")
@@ -29,10 +37,13 @@ class OwnProfileViewModel : ViewModel() {
     get() = _interests
 
   init {
-    _profile = ProfileFirebaseConnection().dummyFetch()
-    _username.value = _profile.userName
-    _bio.value = _profile.bio
-    _image.value = _profile.image
+    runBlocking {
+
+      _profile = ProfileFirebaseConnection().fetch(Firebase.auth.currentUser!!.uid)!!
+      _username.value = _profile.userName
+      _bio.value = _profile.bio
+      _image.value = _profile.image
+    }
   }
 
   fun save() {
