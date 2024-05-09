fdc771e2ae2643d1d21be0e38aba3a68ad437bba
Mathunter <57802139+Mathunter2@users.noreply.github.com>
2024-05-09 17:04:11 +0200

    getting closer

--- app/src/main/java/com/github/se/gatherspot/model/MapViewModel.kt
+++ app/src/main/java/com/github/se/gatherspot/model/MapViewModel.kt
@@ -4,26 +4,42 @@ import android.Manifest
 import android.app.Application
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationRequest
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.github.se.gatherspot.firebase.EventFirebaseConnection
+import com.github.se.gatherspot.model.event.Event
+import com.google.android.gms.location.CurrentLocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.runBlocking
 
+@RequiresApi(Build.VERSION_CODES.S)
 class MapViewModel(application: Application) : AndroidViewModel(application) {
 
     private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(application.applicationContext)
     private val _currentLocation = MutableLiveData<LatLng>()
 
+    private val _events = MutableLiveData<MutableList<Event>>()
+
     val currentLocation: LiveData<LatLng>
         get() = _currentLocation
+    val events: LiveData<MutableList<Event>>
+        get() = _events
+
 
     init {
         fetchLocation()
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     private fun fetchLocation() {
+        Log.d("MapViewModel", "fetchLocation")
         if (ActivityCompat.checkSelfPermission(
                 getApplication(),
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -33,13 +49,18 @@ class MapViewModel(application: Application) : AndroidViewModel(application) {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            fusedLocationClient.getCurrentLocation(LocationRequest.QUALITY_HIGH_ACCURACY, null) .addOnSuccessListener { location: Location? ->
                 location?.let {
                     _currentLocation.value = LatLng(it.latitude, it.longitude)
                 }
             }
         }
     }
+
+    suspend fun fetchEvents(): MutableList<Event> {
+        return EventFirebaseConnection().fetchRegisteredTo()
+    }
+
 }
 
 
--- app/src/main/java/com/github/se/gatherspot/ui/Map.kt
+++ app/src/main/java/com/github/se/gatherspot/ui/Map.kt
@@ -4,6 +4,9 @@ import android.Manifest
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,24 +15,44 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import com.github.se.gatherspot.MainActivity
+import com.github.se.gatherspot.R
+import com.github.se.gatherspot.firebase.EventFirebaseConnection
+import com.github.se.gatherspot.firebase.IdListFirebaseConnection
 import com.github.se.gatherspot.model.MapViewModel
+import com.github.se.gatherspot.model.event.Event
 import com.github.se.gatherspot.ui.navigation.BottomNavigationMenu
 import com.github.se.gatherspot.ui.navigation.NavigationActions
 import com.github.se.gatherspot.ui.navigation.TOP_LEVEL_DESTINATIONS
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
 
 private const val DEFAULT_ZOOM_LEVEL = 15f
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun Map(viewModel: MapViewModel, nav: NavigationActions) {
+
+
+    var events: MutableList<Event> = mutableListOf()
+    var init = true
+
+
+    LaunchedEffect(init) {
+        events = viewModel.fetchEvents()
+        init = false
+    }
 
 
     val currentLocation by viewModel.currentLocation.observeAsState(LatLng(0.0, 0.0))
@@ -54,15 +77,24 @@ fun Map(viewModel: MapViewModel, nav: NavigationActions) {
           Log.d("MapAccess", MainActivity.mapAccess.toString())
           MainActivity.mapLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
       }
-        GoogleMap(
-            properties = MapProperties(mapType = MapType.HYBRID),
-            modifier = Modifier.testTag("Google Map"),
-            contentPadding = paddingValues,
-            cameraPositionState = cameraPositionState,
-        ) {
+      GoogleMap(
+          properties = MapProperties(mapType = MapType.HYBRID),
+          modifier = Modifier.testTag("Google Map"),
+          contentPadding = paddingValues,
+          cameraPositionState = cameraPositionState,
+      ) {
           Marker(
               state = MarkerState(currentLocation),
-              title = "Your current position")
-        }
+              title = "Your current position"
+          )
+          for (event in events) {
+              Marker(
+                  state = MarkerState(LatLng(event.location?.latitude?:0.0, event.location?.longitude?:0.0)),
+                  title = event.title
+              )
+          }
       }
+
+
+  }
 }
