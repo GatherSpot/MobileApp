6fb549bb359047fbf09070236db9650f313a61ad
mmalonga <melvin.malonga-matouba@epfl.ch>
2024-05-10 17:56:57 +0200

    Facilitate aggregation part 3 : rating is in its own field and document is named after uid

--- app/src/main/java/com/github/se/gatherspot/firebase/RatingFirebaseConnection.kt
+++ app/src/main/java/com/github/se/gatherspot/firebase/RatingFirebaseConnection.kt
@@ -3,45 +3,44 @@ package com.github.se.gatherspot.firebase
 import android.util.Log
 import com.github.se.gatherspot.model.Rating
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.FieldValue
-import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.firestore
 import kotlin.coroutines.resume
 import kotlinx.coroutines.suspendCancellableCoroutine
 
 class RatingFirebaseConnection {
-  val COLLECTION = FirebaseCollection.EVENT_RATINGS.toString().lowercase()
-  val TAG = "RatingFirebaseConnection"
+  private val COLLECTION = FirebaseCollection.EVENT_RATINGS.toString().lowercase()
+  private val TAG = "RatingFirebaseConnection"
 
-    private fun attendeesRatingsCollection(eventID: String) =
-        Firebase.firestore.collection(COLLECTION).document(eventID)
-            .collection("attendees_ratings")
+  private fun attendeesRatingsCollection(eventID: String) =
+      Firebase.firestore.collection(COLLECTION).document(eventID).collection("attendees_ratings")
   /**
    * Fetches the rating of the user for the event
    *
    * @param eventID the id of the event
-   * @param uid the id of the user
+   * @param userID the id of the user
    * @return the rating of the user for the event if the user has not rated the event, returns
    *   UNRATED if the event doesn't have any ratings, or that get results in failure, returns null
    */
-  suspend fun fetchRating(eventID: String, uid: String): Rating? =
+  suspend fun fetchRating(eventID: String, userID: String): Rating? =
       suspendCancellableCoroutine { continuation ->
-        var rating: Rating? = Rating.UNRATED
+        var rating: Rating?
 
-        attendeesRatingsCollection(eventID)
-            .document(eventID)
+          attendeesRatingsCollection(eventID)
+            .document(userID)
             .get()
             .addOnSuccessListener { document ->
               if (document != null) {
-                if (document.get(uid) != null) {
-                  rating = Rating.fromLong(document.get(uid)!! as Long)
-                  Log.d(TAG, "User $uid rated the event $eventID as ${rating}")
+                if (document.get("rating") != null) {
+                  rating = Rating.fromLong(document.get("rating")!! as Long)
+                  Log.d(TAG, "User $userID rated the event $eventID as $rating")
+                  continuation.resume(rating)
+                } else {
+                  continuation.resume(null)
                 }
               } else {
                 Log.d(TAG, "No such rating")
                 continuation.resume(null)
               }
-              continuation.resume(rating)
             }
             .addOnFailureListener { exception ->
               Log.d(TAG, "get failed with :", exception)
@@ -57,13 +56,15 @@ class RatingFirebaseConnection {
    */
   suspend fun fetchRatings(eventID: String): Map<String, Rating>? =
       suspendCancellableCoroutine { continuation ->
-          attendeesRatingsCollection(eventID)
-            .document(eventID)
+        attendeesRatingsCollection(eventID)
             .get()
-            .addOnSuccessListener { document ->
-              val ratings: Map<String, Rating>? =
-                  document.data?.mapValues { Rating.fromLong(it.value as Long) }
-              Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+            .addOnSuccessListener { documents ->
+              val ratings = mutableMapOf<String, Rating>()
+              for (document in documents) {
+                if (document.get("rating") != null) {
+                  ratings[document.id] = Rating.fromLong(document.get("rating")!! as Long)
+                }
+              }
               continuation.resume(ratings)
             }
             .addOnFailureListener { exception ->
@@ -81,35 +82,24 @@ class RatingFirebaseConnection {
    *   the rating is deleted
    */
   fun update(eventID: String, userID: String, rating: Rating) {
-
-    var data: Map<String, Any> = mapOf(userID to FieldValue.delete())
     if (rating != Rating.UNRATED) {
-      data = mapOf(userID to Rating.toLong(rating))
+      Firebase.firestore.collection(COLLECTION).document(eventID).set(mapOf("eventID" to eventID))
+
+      val data: Map<String, Any> = mapOf("rating" to Rating.toLong(rating))
+
+      attendeesRatingsCollection(eventID)
+          .document(userID)
+          .set(data)
+          .addOnSuccessListener {
+            Log.d(TAG, "added Rating of event $eventID, of $rating for user $userID")
+          }
+          .addOnFailureListener { e -> Log.w(TAG, "Error adding document", e) }
+    } else {
+      attendeesRatingsCollection(eventID)
+          .document(userID)
+          .delete()
+          .addOnSuccessListener { Log.d(TAG, "Deleted rating of user $userID for event $eventID") }
+          .addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
     }
-
-      attendeesRatingsCollection(eventID)
-        .document(eventID)
-        .set(data, SetOptions.merge())
-        .addOnSuccessListener {
-          Log.d(TAG, "added Rating of event $eventID, of $rating for user $userID")
-        }
-        .addOnFailureListener { e -> Log.w(TAG, "Error adding document", e) }
-  }
-
-  /**
-   * Deletes all the ratings of the event
-   *
-   * @param eventID the id of the event
-   *
-   * Meant to be ran :
-   * - in test functions for convenience
-   * - typically after a passed event is deleted even though atm we don't do that
-   */
-  fun deleteRatings(eventID: String) {
-      attendeesRatingsCollection(eventID)
-        .document(eventID)
-        .delete()
-        .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully deleted!") }
-        .addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
   }
 }
