8e566de160e90a290cc67ee8beca5452b018f758
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2024-05-15 11:48:26 +0200

    re-add this test

--- app/src/androidTest/java/com/github/se/gatherspot/firebase/ProfileFirebaseConnectionTest.kt
+++ app/src/androidTest/java/com/github/se/gatherspot/firebase/ProfileFirebaseConnectionTest.kt
@@ -71,22 +71,20 @@ class ProfileFirebaseConnectionTest {
     }
   }
 
-  // Isolate it for now as it is not working
-  /*
-    @Test
-    fun testDelete() {
-      runTest {
-        var fetched: Profile? = null
-        profileFirebaseConnection.add(toAdd)
-        async { fetched = profileFirebaseConnection.fetch(toAdd.id) }.await()
-        assertNotNull(fetched)
-        profileFirebaseConnection.delete(toAdd.id)
+  @Test
+  fun testDelete() {
+    runTest {
+      var fetched: Profile? = null
+      profileFirebaseConnection.add(toAdd)
+      async { fetched = profileFirebaseConnection.fetch(toAdd.id) }.await()
+      assertNotNull(fetched)
+      profileFirebaseConnection.delete(toAdd.id)
 
-        async { fetched = profileFirebaseConnection.fetch(toAdd.id) }.await()
-        assertEquals(null, fetched)
-      }
+      async { fetched = profileFirebaseConnection.fetch(toAdd.id) }.await()
+      assertEquals(null, fetched)
     }
-  */
+  }
+
   @Test
   fun testGetCurrentUserUid() {
     runTest {
