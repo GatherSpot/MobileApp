b7c9a30c8a192ccccae68cd26ba3f0e56911fced
mmalonga <melvin.malonga-matouba@epfl.ch>
2024-05-16 17:12:12 +0200

    compiles

--- app/src/main/java/com/github/se/gatherspot/ui/event/CalendarReminderGenerator.kt
+++ app/src/main/java/com/github/se/gatherspot/ui/event/CalendarReminderGenerator.kt
@@ -1,5 +1,6 @@
 package com.github.se.gatherspot.ui.event
 
+import android.content.Context
 import android.content.Intent
 import android.provider.CalendarContract
 import android.provider.CalendarContract.Events
@@ -8,25 +9,25 @@ import java.util.Calendar
 
 class CalendarReminderGenerator {
     companion object {
-        fun generateCalendarReminder(event : Event) {
+        fun generateCalendarReminder(context: Context, event : Event) {
 
-            Intent(Intent.ACTION_INSERT, Events.CONTENT_URI).apply {
+            val intent = Intent(Intent.ACTION_INSERT, Events.CONTENT_URI).apply {
                 val beginTime: Calendar = Calendar.getInstance().apply {
                     set(
                         event.eventStartDate!!.year,
-                        event.eventStartDate!!.monthValue,
-                        event.eventStartDate!!.dayOfMonth,
+                        event.eventStartDate.monthValue,
+                        event.eventStartDate.dayOfMonth,
                         event.timeBeginning!!.hour,
-                        event.timeBeginning!!.minute
+                        event.timeBeginning.minute
                     )
                 }
                 val endTime = Calendar.getInstance().apply {
                     set(
                         event.eventEndDate!!.year,
-                        event.eventEndDate!!.monthValue,
-                        event.eventEndDate!!.dayOfMonth,
+                        event.eventEndDate.monthValue,
+                        event.eventEndDate.dayOfMonth,
                         event.timeEnding!!.hour,
-                        event.timeEnding!!.minute
+                        event.timeEnding.minute
                     )
                 }
                 putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.timeInMillis)
@@ -37,25 +38,8 @@ class CalendarReminderGenerator {
 
             }
 
-            startActivity(intent)
+            context.startActivity(intent)
 
-            /*
-            val intent = Intent(Intent.ACTION_INSERT, Events.CONTENT_URI)
-            intent.setData(android.provider.CalendarContract.Events.CONTENT_URI)
-            intent.putExtra(CalendarContract.Events.TITLE, event.title)
-            intent.putExtra(CalendarContract.Events.DESCRIPTION, event.description)
-            val startTime : Calendar = Calendar.getInstance()
-            startTime.clear()
-            event.eventStartDate?.let {
-                startTime.set(it.year, it.monthValue, it.dayOfMonth)
-            }
-            event.timeBeginning?.let {
-                startTime.set(Calendar.HOUR_OF_DAY, it.hour)
-                startTime.set(Calendar.MINUTE, it.minute)
-            }
-            startActivity( intent)
-
-             */
 
 
         }
