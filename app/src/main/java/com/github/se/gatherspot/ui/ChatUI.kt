205d977232fe0a798062452548c9fc49c109f824
Wes <wesley.nanadavies@epfl.ch>
2024-05-29 12:28:00 +0200

    improved ui for chats (timestamp + sender displayed)

--- app/src/main/java/com/github/se/gatherspot/ui/ChatUI.kt
+++ app/src/main/java/com/github/se/gatherspot/ui/ChatUI.kt
@@ -1,6 +1,9 @@
 package com.github.se.gatherspot.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -26,6 +30,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,9 +42,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+import com.github.se.gatherspot.firebase.ProfileFirebaseConnection
 import com.github.se.gatherspot.model.chat.ChatMessage
 import com.github.se.gatherspot.model.chat.ChatViewModel
 import com.github.se.gatherspot.ui.navigation.NavigationActions
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 import kotlinx.coroutines.launch
 
@@ -88,7 +95,9 @@ fun ChatUI(viewModel: ChatViewModel, currentUserId: String, navActions: Navigati
               }
               items(messages) { message ->
                 ChatMessageCard(
-                    message = message, isCurrentUser = message.senderId == currentUserId)
+                    message = message, isCurrentUser = message.senderId == currentUserId) {
+                      ProfileFirebaseConnection().fetch(message.senderId)!!.userName
+                    }
               }
               // Add a small space at the end of the list
               item { Spacer(modifier = Modifier.height(8.dp).testTag("spacer")) }
@@ -99,11 +108,20 @@ fun ChatUI(viewModel: ChatViewModel, currentUserId: String, navActions: Navigati
 /**
  * Composable for a chat message card.
  *
+ * @param ID of the sender
  * @param message The chat message
  * @param isCurrentUser Whether the message is from the current user
  */
 @Composable
-fun ChatMessageCard(message: ChatMessage, isCurrentUser: Boolean) {
+fun ChatMessageCard(
+    message: ChatMessage,
+    isCurrentUser: Boolean,
+    findUsername: suspend () -> String
+) {
+  var username by remember { mutableStateOf("") }
+
+  LaunchedEffect(message.id) { username = findUsername() }
+
   val backgroundColor = if (isCurrentUser) Color(0xFF90EE90) else Color.White
   val padding =
       if (isCurrentUser) PaddingValues(end = 16.dp, top = 4.dp, bottom = 4.dp)
@@ -112,15 +130,31 @@ fun ChatMessageCard(message: ChatMessage, isCurrentUser: Boolean) {
   Box(
       modifier = Modifier.fillMaxWidth().padding(padding).testTag("boxChatMessageCard"),
       contentAlignment = if (isCurrentUser) Alignment.CenterEnd else Alignment.CenterStart) {
-        Card(
-            backgroundColor = backgroundColor,
-            elevation = 4.dp,
-            modifier = Modifier.padding(4.dp).testTag("chatMessageCard")) {
-              Text(
-                  text = message.message,
-                  style = MaterialTheme.typography.body2,
-                  modifier = Modifier.padding(8.dp))
-            }
+        Column(verticalArrangement = Arrangement.Center) {
+          Card(
+              backgroundColor = backgroundColor,
+              elevation = 4.dp,
+              modifier =
+                  Modifier.padding(4.dp)
+                      .testTag("chatMessageCard")
+                      // Makes sure the text occupies space nicely,
+                      // either on the left or right side
+                      .widthIn(max = 200.dp)) {
+                Text(
+                    text = message.message,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(8.dp))
+              }
+          Text(
+              text =
+                  "$username on the ${
+                        message.timestamp.format(
+                            DateTimeFormatter.ofPattern("d MMM yyyy, HH:mm")
+                        )
+                    } ",
+              style = MaterialTheme.typography.body2,
+              modifier = Modifier.clickable {})
+        }
       }
 }
 
